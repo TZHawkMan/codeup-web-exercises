40d1d299fabf2331bc8b56f2ec8c0fb3d0c23279
Tianrun Zhang <zhang_tianrun@yahoo.com>
2022-10-14 16:05:49 -0500

    array exercise 1.0

---
+++ js/In-class-mini-exercises.js
@@ -0,0 +1,26 @@
+//Create a variable called num that is equal to 0.
+// Write a while loop that increments "num" by 5 so long as num is less
+// than 100. Do the same thing with a do-while.
+
+//let num=(0);
+//while(num < 100){
+//    console.log (num);
+//    num +=5
+//}
+
+
+//do{ console.log(num);
+ //   num +=5;
+
+//}while (num < 100);
+
+
+for (let num =0; num < 100; num+=5){
+    console.log (num);
+}
+
+let hello = "hello";
+for (let i =0; i< hello.lenth; i++){
+    //console.log(hello.charAt(i))
+    //console.log (hello[i])
+}
---
+++ js/for_loops.js
@@ -0,0 +1,76 @@
+(function() {
+"use strict"
+
+//2.Create a function named showMultiplicationTable that accepts a number and console.logs the multiplication table for that number (just multiply by the numbers 1 through 10)
+
+function showMultiplicationTable(x){
+    let y = 1
+    while (y <= 10){
+        console.log(x + "*" + y + "=" + (x*y));
+        y++
+    }
+}
+
+showMultiplicationTable(7)
+
+
+
+
+
+//3.Use a for loop and the code from the previous lessons to generate 10 random numbers between 20 and 200 and output to the console whether each number is odd or even.
+
+
+
+
+
+//4.Create a for loop that uses console.log to create the output shown below.
+//     1
+//     22
+//     333
+//     4444
+//     55555
+//     666666
+//     7777777
+//     88888888
+//     999999999
+
+//5.Create a for loop that uses console.log to create the output shown below.
+//     100
+//     95
+//     90
+//     85
+//     80
+//     75
+//     70
+//     65
+//     60
+//     55
+//     50
+//     45
+//     40
+//     35
+//     30
+//     25
+//     20
+//     15
+//     10
+//     5
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+})()
