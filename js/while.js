2fd11dd836ce6a174867f42525e51bf66579fe2f
Tianrun Zhang <zhang_tianrun@yahoo.com>
2022-10-19 23:28:05 -0500

    updated

--- js/while.js
+++ js/while.js
@@ -16,8 +16,15 @@
 // 32768
 // 65536
 
-let x = 2 ;
+let x = 1 ;
 while (x < 65536){
-    console.log("while loop iteration number" + x);
-    x++;
+    console.log(x *= 2);
+
 }
+
+
+//2.An ice cream seller can't go home until she sells all of her cones. First write enough code that generates a random number between 50 and 100 representing the amount of cones to sell before you start your loop. Inside of the loop your code should generate another random number between 1 and 5, simulating the amount of cones being bought by her clients. Use a do-while loop to log to the console the amount of cones sold to each person. The below code shows how to get the random numbers for this exercise.
+// This is how you get a random number between 50 and 100
+var allCones = Math.floor(Math.random() * 50) + 50;
+// This expression will generate a random number between 1 and 5
+Math.floor(Math.random() * 5) + 1;
