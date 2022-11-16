e9e289367efa037a1c67f624a0a6ee1bdf4012e8
Tianrun Zhang <zhang_tianrun@yahoo.com>
2022-11-16 16:20:44 -0600

    konami broken codes

---
+++ js/konami.js
@@ -0,0 +1,48 @@
+"use strict";
+(function () {
+
+    let keyCode = [38,38,40,40,37,39,37,39,66,65,13]
+    //keyCodes = [up,up,down,down,left,right,left,right,b,a,enter]
+
+
+
+
+
+        $(document).keyup(function(event) {
+            if (event.which == 38){
+
+            };
+            console.log(event.which)
+
+            alert('30 lives!');
+        });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+})();
