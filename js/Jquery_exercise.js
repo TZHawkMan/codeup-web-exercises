f2af67b67a9d6eb7b54f92b85dc445e745b6c1c6
Tianrun Zhang <zhang_tianrun@yahoo.com>
2022-11-16 12:14:31 -0600

    jquery-event exercise

--- js/Jquery_exercise.js
+++ js/Jquery_exercise.js
@@ -19,20 +19,38 @@
          //$('.codeup').css('border', '1px').css('background-color', '#ff0000');
 
      //element selectors
-         $('li').css('background-color', 'yellow').css('font-size', '20px');
-         $('p').css('background-color', 'yellow');
-         $('h1').css('background-color', 'yellow')
-         var contents = $('h1').html();
-         alert(contents);
+     //     $('li').css('background-color', 'yellow').css('font-size', '20px');
+     //     $('p').css('background-color', 'yellow');
+     //     $('h1').css('background-color', 'yellow')
+     //     var contents = $('h1').html();
+     //     alert(contents);
 
 
      // mutiple selectors
-     $(" li, p,h1").css( 'background-color','blue');
+     // $(" li, p,h1").css( 'background-color','blue');
+
+         $('h1').click(function() {
+             $('h1').css('background-color', 'green');
+
+         });
+        $('p').dblclick(function(){
+            $('p').css('font-size','18px');
+        });
+
+
+         $('li').hover(
+             function() {
+                 $(this).css('color', 'red');
+             },
+             function() {
+                 $(this).css('color', 'black');
+             }
+         );
 
 
 
 
-     })
+      })
 
 
 })()
