681cef7e609ef92ab2d048b92756590323a8198b
Tianrun Zhang <zhang_tianrun@yahoo.com>
2022-11-28 23:53:52 -0600

    30 live added

--- js/konami.js
+++ js/konami.js
@@ -1,6 +1,6 @@
 "use strict";
 (function () {
-
+    let cheatCode=[]
     let keyCode = [38,38,40,40,37,39,37,39,66,65,13]
     //keyCodes = [up,up,down,down,left,right,left,right,b,a,enter]
 
@@ -9,17 +9,16 @@
 
 
         $(document).keyup(function(event) {
-            if (event.which == 38){
+            console.log(event.keyCode);
+            cheatCode.push(event.keyCode)
+            cheatCode.splice(-keyCode.length -1, cheatCode.length - keyCode.length)
+            console.log(cheatCode)
 
-            };
-            console.log(event.which)
+            if(cheatCode.join('').includes(keyCode.join(''))) {
+                alert('You have added 30 lives')
 
-            alert('30 lives!');
+            }
         });
-
-
-
-
 
 
 
